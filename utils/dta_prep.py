d6fe84a7d8cdf243ccda8e930108345c06704571
PeterDingVN <dingthanhthe@gmail.com>
2025-08-19 10:56:18 +0700

    Modify utils.dta_prep.py

--- utils/dta_prep.py
+++ utils/dta_prep.py
@@ -1,47 +1,58 @@
 from .libs import *
 from sklearn.impute import KNNImputer
 
-# EDA function describe + skew, kurtosis
-def eda_describe(df, columns=None):
 
-  if not columns:
-    # select only numeric col
-    num_col1 = df.select_dtypes(include='number').columns
+def eda_describe(df: pd.DataFrame) -> pd.DataFrame:
+  '''
+  param df: input a dataframe df or df[[...]]
+  return: summary table of stats
 
-    # describe function
-    df_describe = df[num_col1].describe().round(4)
+  Example:
+    eda_describe(df[['col1', 'col2']])
+    -> return:
+            mean    25%     50%     75%       ....
+    col 1    20     10      19      25
+    col 2    25     ...
+  '''
 
-    # check for kurtosis and skewness
-    sk = stats.skew(df[num_col1], nan_policy='omit')
-    k = stats.kurtosis(df[num_col1], nan_policy='omit')
+  num_col1 = df.select_dtypes(include='number').columns
 
-    sk_k = pd.DataFrame([sk, k], columns = num_col1)
-    sk_k = sk_k.apply(lambda x: round(x,4))
+  df_describe = df[num_col1].describe().round(4)
 
-    result = pd.concat([df_describe, sk_k])
-    result.rename(index = {0: 'skewness', 1: 'kurtosis'}, inplace = True)
+  sk = stats.skew(df[num_col1], nan_policy='omit')
+  k = stats.kurtosis(df[num_col1], nan_policy='omit')
 
-  else:
-    # select only numeric col
-    num_col1 = df[columns].select_dtypes(include='number').columns
+  sk_k = pd.DataFrame([sk, k], columns = num_col1)
+  sk_k = sk_k.apply(lambda x: round(x,4))
 
-    # describe function
-    df_describe = df[num_col1].describe().round(4)
-
-    # check for kurtosis and skewness
-    sk = stats.skew(df[num_col1], nan_policy='omit')
-    k = stats.kurtosis(df[num_col1], nan_policy='omit')
-
-    sk_k  = pd.DataFrame([sk,k], columns = num_col1)
-    sk_k = sk_k.apply(lambda x: round(x,4))
-
-    result = pd.concat([df_describe, sk_k])
-    result.rename(index = {0: 'skewness', 1: 'kurtosis'}, inplace = True)
-
-
+  result = pd.concat([df_describe, sk_k])
+  result.rename(index = {0: 'skewness', 1: 'kurtosis'}, inplace = True)
   return result.T
 
-def select_data(df):
+
+
+def select_data(df: pd.DataFrame) -> pd.DataFrame:
+  '''
+  param: dataframe -> df or df[[]]
+  return: new dataframe with only companies satisfying the condition
+
+  Example:
+    A   B    C    D
+    1   1    NaN  NaN
+    1   4    NaN  NaN
+    1   7    3    7
+  NaN   6    5    10
+
+  Return -> only row 3 and 4 are kept because NaN < 2 condition
+    A   B    C    D
+    1   7    3    7
+  NaN   6    5    10
+
+  Note: the func automatically set min NaN <= 6 and len(year) >=4, meaning companies
+  with data on at least 4 consecutive years and each obs has no more than 6 NaN would be chosen
+
+  '''
+
   data = []
   try:
     df = df[df.isna().sum(axis=1) <= 6]
@@ -52,18 +63,38 @@ def select_data(df):
         data.append(grp)
     data_fin = pd.concat(data)
     return data_fin
+
   except ValueError as e:
     return e
 
-def impute(df):
-  # requirement
+
+
+def impute(df: pd.DataFrame) -> pd.DataFrame:
+  '''
+  param df: dataframe -> df or df[[]]
+  return: fully filled dataframe using KNN
+
+  Example:
+    A   B    C    D
+    1   1    NaN  NaN
+    1   4    NaN  NaN
+    1   7    3    7
+  NaN   6    5    10
+
+  Output:
+    A   B    C    D
+    1   1    4    5
+    1   4    7    2
+    1   7    3    7
+    1   6    5    10
+
+  '''
+
   df_filled = df.copy()
   num_cols = df.select_dtypes(include='number').columns
 
-  # set up
   imputer = KNNImputer(n_neighbors=3, weights='distance')
 
-  # Fill
   df_filled[num_cols] = imputer.fit_transform(df_filled[num_cols])
 
   return df_filled
